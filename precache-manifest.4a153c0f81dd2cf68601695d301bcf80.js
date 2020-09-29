0b0e11160c6458594c53cbe37c9dc0d7ad492d19
bue221 <camiloplaza3@gmail.com>
2020-09-28 19:55:14 -0500

    Updates

---
+++ precache-manifest.4a153c0f81dd2cf68601695d301bcf80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2d2a2601229739430d68d36c9ec57b3",
+    "url": "/first-steps-ReactAdmin/index.html"
+  },
+  {
+    "revision": "c81b50eb536fbc86538a",
+    "url": "/first-steps-ReactAdmin/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0cc44025791c9305b2ca",
+    "url": "/first-steps-ReactAdmin/static/js/2.97eb1425.chunk.js"
+  },
+  {
+    "revision": "980b85962847aa8967aae5074f7fd5d5",
+    "url": "/first-steps-ReactAdmin/static/js/2.97eb1425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c81b50eb536fbc86538a",
+    "url": "/first-steps-ReactAdmin/static/js/main.b5ac1d16.chunk.js"
+  },
+  {
+    "revision": "b724e077de33760bf8a3",
+    "url": "/first-steps-ReactAdmin/static/js/runtime-main.b73303b8.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/first-steps-ReactAdmin/precache-manifest.4a153c0f81dd2cf68601695d301bcf80.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/first-steps-ReactAdmin/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
